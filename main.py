503fdd91527390f30331c447d8d455c76e69aaf6
Piotr <piotrusk8@gmail.com>
2026-03-28 20:35:35 +0100

    Add secrets management with sops

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 import argparse
+import yaml
 from dotenv import load_dotenv
 from settings import Settings
 
@@ -6,6 +8,11 @@ from settings import Settings
 def export_envs(environment: str = "dev") -> None:
     env_file = f"config/.env.{environment}"
     load_dotenv(env_file)
+
+    with open("secrets.yaml") as f:
+        secrets = yaml.safe_load(f)
+    for key, value in secrets.items():
+        os.environ[key] = str(value)
 
 
 if __name__ == "__main__":
@@ -26,3 +33,4 @@ if __name__ == "__main__":
 
     print("APP_NAME: ", settings.APP_NAME)
     print("ENVIRONMENT: ", settings.ENVIRONMENT)
+    print("API_KEY: ", settings.API_KEY)
